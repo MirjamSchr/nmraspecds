8f7aeab7a45b3bbf1e3648ca60c6acdd3e2aa6de
Mirjam Schröder <post@mirjam-schroeder.de>
2024-01-06 23:59:35 +0100

    First import using nmrglue

--- nmraspecds/io.py
+++ nmraspecds/io.py
@@ -1,5 +1,42 @@
 """
 io module of the nmraspecds package.
 """
+import aspecd.io
+import nmrglue
 
 
+class BrukerImporter(aspecd.io.DatasetImporter):
+    """
+    One sentence (on one line) describing the class.
+
+    More description comes here...
+
+
+    Attributes
+    ----------
+    attr : :class:`None`
+        Short description
+
+    Raises
+    ------
+    exception
+        Short description when and why raised
+
+
+    Examples
+    --------
+    It is always nice to give some examples how to use the class. Best to do
+    that with code examples:
+
+    .. code-block::
+
+        obj = BrukerImporter()
+        ...
+
+    
+
+    """
+
+    def _import(self):
+        parameters, data = nmrglue.bruker.read(self.source)
+        self.dataset.data.data = data
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,4 +1,18 @@
 import unittest
 
+import nmraspecds.dataset
 from nmraspecds import io
 
+
+class TestBrukerImporter(unittest.TestCase):
+    def setUp(self):
+        self.bruker_importer = io.BrukerImporter()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_import_data_to_dataset(self):
+        dataset = nmraspecds.dataset.ExperimentalDataset()
+        self.bruker_importer.source = 'testdata/Adamantane/1'
+        dataset.import_from(self.bruker_importer)
+        self.assertTrue(dataset.data.data.any())
