4ed8ed5e5e4ef84d137a1fc29d19dea46f3fdd79
Mirjam Schröder <code@mirjam-schroeder.de>
2024-04-21 11:31:20 +0200

    Automatically invert chemical shift axis. No documentation by now.

--- nmraspecds/plotting.py
+++ nmraspecds/plotting.py
@@ -1,3 +1,34 @@
 """
 plotting module of the nmraspecds package.
 """
+import aspecd.plotting
+
+
+class SinglePlotter1D(aspecd.plotting.SinglePlotter1D):
+    def __init__(self):
+        super().__init__()
+        self.properties.axes.invert = "x"
+
+
+class SinglePlotter2D(aspecd.plotting.SinglePlotter2D):
+    def __init__(self):
+        super().__init__()
+        self.properties.axes.invert = "x"
+
+
+class SinglePlotter2DStacked(aspecd.plotting.SinglePlotter2DStacked):
+    def __init__(self):
+        super().__init__()
+        self.properties.axes.invert = "x"
+
+
+class MultiPlotter1D(aspecd.plotting.MultiPlotter1D):
+    def __init__(self):
+        super().__init__()
+        self.properties.axes.invert = "x"
+
+
+class MultiPlotter1DStacked(aspecd.plotting.MultiPlotter1DStacked):
+    def __init__(self):
+        super().__init__()
+        self.properties.axes.invert = "x"
--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,3 +1,64 @@
 import unittest
 
-from nmraspecds import plotting
+from nmraspecds import plotting, dataset
+import numpy as np
+
+
+class TestSinglePlotter1D(unittest.TestCase):
+    def setUp(self):
+        self.plotter = plotting.SinglePlotter1D()
+        self.dataset = dataset.ExperimentalDataset()
+        self.dataset.data.data = np.random.random(5)
+        self.plotter.dataset = self.dataset
+
+    def test_axis_is_inverted(self):
+        self.plotter.plot()
+        self.assertTrue(self.plotter.axes.xaxis_inverted())
+
+
+class TestSinglePlotter2D(unittest.TestCase):
+    def setUp(self):
+        self.plotter = plotting.SinglePlotter2D()
+        self.dataset = dataset.ExperimentalDataset()
+        self.dataset.data.data = np.random.random((5, 3))
+        self.plotter.dataset = self.dataset
+
+    def test_axis_is_inverted(self):
+        self.plotter.plot()
+        self.assertTrue(self.plotter.axes.xaxis_inverted())
+
+
+class TestSinglePlotter2DStacked(unittest.TestCase):
+    def setUp(self):
+        self.plotter = plotting.SinglePlotter2DStacked()
+        self.dataset = dataset.ExperimentalDataset()
+        self.dataset.data.data = np.random.random((5, 3))
+        self.plotter.dataset = self.dataset
+
+    def test_axis_is_inverted(self):
+        self.plotter.plot()
+        self.assertTrue(self.plotter.axes.xaxis_inverted())
+
+
+class TestMultiPlotter1D(unittest.TestCase):
+    def setUp(self):
+        self.plotter = plotting.MultiPlotter1D()
+        self.dataset = dataset.ExperimentalDataset()
+        self.dataset.data.data = np.random.random(5)
+        self.plotter.datasets = [self.dataset, self.dataset]
+
+    def test_axis_is_inverted(self):
+        self.plotter.plot()
+        self.assertTrue(self.plotter.axes.xaxis_inverted())
+
+
+class TestMultiPlotter1DStacked(unittest.TestCase):
+    def setUp(self):
+        self.plotter = plotting.MultiPlotter1DStacked()
+        self.dataset = dataset.ExperimentalDataset()
+        self.dataset.data.data = np.random.random(5)
+        self.plotter.datasets = [self.dataset, self.dataset]
+
+    def test_axis_is_inverted(self):
+        self.plotter.plot()
+        self.assertTrue(self.plotter.axes.xaxis_inverted())
